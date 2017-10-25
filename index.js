fb9728a7da3fb27db3d3f4a81b69f4c54aaa53ac
Floppy <nastyakogalim@gmail.com>
2017-10-26 02:16:19 +0300

    'last'

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ router.get('/verify/:token', require('./routes/verify').get);
 app.use(router.routes());
 
 const socket = require('./libs/socket');
-const server = app.listen(3000);
+const server = app.listen(config.get('port'));
 socket(server);
